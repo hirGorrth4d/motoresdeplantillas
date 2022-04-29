d4bed15eb261abc3dc5e8a037e21fb06b81781d1
sonovdragons <cagliolom@gmail.com>
2022-04-29 12:41:37 -0300

    plantillas

---
+++ PlantillaEjs/index.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express ()
+let products =  [
+    {
+        "product": "Iphone 12",
+        "price": 800
+    },
+    {
+        "product": "Macbook Pro 14",
+        "price": 1600
+    },
+    {
+        "product": "Apple Watch",
+        "price": 600
+    },
+    {
+        "product": "Macbook Air",
+        "price": 1000
+    },
+    {
+        "product": "Ipad",
+        "price": 900
+    }
+]
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use(express.static("public"))
+app.set("views", "./views")
+app.set("view engine", "ejs")
+
+app.get("/", (req,res) =>{
+    res.render("index", {titulo: "Carga tu producto"})
+})
+app.get("/productos", (req,res) =>{
+    res.render("productos", {data: products})
+})
+app.post("/productos", (req,res) =>{
+    let {product, price} = req.body
+    let newProduct = {
+        product: product,
+        price: price
+    }
+    products.push(newProduct)
+    res.render("productos", 
+        {data: products
+        })
+})
+
+app.listen(8080, ()=>{
+    console.log("server is running on port 8080")
+})
---
+++ README.md
@@ -0,0 +1,4 @@
+# Motores de plantillas
+## Despues de probar los motores de plantillas, en el que mas comodo me en sentido es en Handlebars, ya que la sintaxis para escribir en html es la mas parecida a javascript.
+## En cambio en EJS, es mas complicado, a mi gusto el llamar a las variables y bucles en la plantilla html
+## y en Pug hay que reestructurar la cabeza para no pensar en cerrar los tags de las etiquetas para acostumbrase a la sintaxis
